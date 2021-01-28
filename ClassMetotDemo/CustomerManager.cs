e5e391d35dacc1c2ff0cb95ae49bf6dcaf91df46
Buğra Arslan <bgra.arsln@hotmail.com>
2021-01-28 22:40:41 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void List()
+        {
+            Console.WriteLine("Müşteri listelendi");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Müşteri silindi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.TcNo = "12345678910";
+            customer.FirstName = "Buğra";
+            customer.LastName = "Arslan";
+
+            string[] customers = new string[]
+            {"Tc kimlik no :" + customer.TcNo, "İsim :" + customer.FirstName,"Soyisim :" +  customer.LastName };
+
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item);
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.List();
+            customerManager.Delete();
+            Console.ReadLine();
+        }
+    }
+}
